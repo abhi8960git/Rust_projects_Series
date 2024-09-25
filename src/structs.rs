0fa7fdf1a4d044eaf6eef9c1ba32009798378f55
Abhishek <abui8994ty@gmail.com>
2024-09-25 16:24:31 +0530

    Custom Structs

--- src/structs.rs
+++ src/structs.rs
@@ -21,3 +21,31 @@ pub fn test_new_person() {
         myperson.first_name, myperson.last_name
     );
 }
+
+
+// Custom Struct
+
+struct Vehicle{
+    manufacturer:String,
+    model :String,
+    year:u16,
+    color:VehicleColor
+}
+
+fn new_vehicle()->Vehicle{
+    let v1 =  Vehicle{
+        manufacturer:"Porshe".to_string(),
+        model:"911".to_string(),
+        year:9233,
+        color:VehicleColor::Green
+    };
+    return  v1;
+}
+
+enum VehicleColor {
+    Silver,
+    Red,
+    Green,
+    Yellow
+}
+
