6243b0e37c69abc250cdc96c95990f9d7a8df0e4
Abhishek <abui8994ty@gmail.com>
2024-09-25 14:45:01 +0530

    rust option type done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::{collections::btree_map::Values, fmt::format};
+use std::{collections::btree_map::Values, fmt::format, result};
 
 use closures::test_closures;
 use match_::test_match;
+use option::{test_option_chartype, test_option_string, test_option_type};
 
-pub mod helpers;
 pub mod closures;
+pub mod helpers;
 pub mod match_;
+pub mod option;
 fn main() {
     println!("Hello, world!");
     // coercion();
@@ -13,52 +15,60 @@ fn main() {
     println!("Hello from {0}", Result);
     // test_if();
     // test_closures();
-    test_match();
-    
+    // test_match();
+    let result = test_option_type();
+    let result1 = test_option_string();
+    let char = test_option_chartype();
+
+    println!("{}", result1.unwrap());
+
+    println!("{0}", result.unwrap());
+
+    if char.is_some() {
+        println!("User has selected a character type");
+        println!("{}", char.unwrap().to_string());
+    } else {
+        println!("character type is None");
+    }
 }
 // type coercion type conversion
 // by default the rust var are immutable
 
-
-fn test_if(){
-    let age_to_drive:u8 = 16u8;
+fn test_if() {
+    let age_to_drive: u8 = 16u8;
 
     println!("Enter te person's age: ");
-    let myinput :&mut String = &mut String::from("");
+    let myinput: &mut String = &mut String::from("");
     std::io::stdin().read_line(myinput).unwrap();
-    
-  let age:u8= myinput.replace("\n","").parse::<u8>().unwrap();
-    if(age >= age_to_drive){
+
+    let age: u8 = myinput.replace("\n", "").parse::<u8>().unwrap();
+    if (age >= age_to_drive) {
         println!("Issueing driver's license , because they are old enough");
     }
 }
 
-fn test_for_loop(){
-    let ages:[i32;5]=[14,18,26,35,41];
-    let age_to_drive:i32 = 16i32;
+fn test_for_loop() {
+    let ages: [i32; 5] = [14, 18, 26, 35, 41];
+    let age_to_drive: i32 = 16i32;
 
-    for value in ages{
-        if value >= age_to_drive{
-            println!(
-                "you are old enough to drive"
-            )
+    for value in ages {
+        if value >= age_to_drive {
+            println!("you are old enough to drive")
         }
     }
 }
 
-fn coercion(){
-    let x:f32 = 255.0;
-    let y:u8 = x as u8 - 5;
+fn coercion() {
+    let x: f32 = 255.0;
+    let y: u8 = x as u8 - 5;
     println!("{:?}", y);
-    let my_str :char = 'A';
+    let my_str: char = 'A';
 
-    let mut first_name:&str= "Trever";
+    let mut first_name: &str = "Trever";
     first_name = "Bob";
 
     // tuple
-    let name:(&str, u8) = ("Happy", 4);
-
+    let name: (&str, u8) = ("Happy", 4);
 }
 
 // anonmoys function and clousure in rust
-
---
+++ src/option.rs
@@ -0,0 +1,36 @@
+pub fn test_option_type()-> Option<u8>{
+let mut opt1: Option<u8>  = None;
+opt1 = Some(8);
+return  opt1;
+}
+
+pub fn test_option_string()-> Option<String>{
+    let mut opt1: Option<String> = None;
+    opt1 = Some("Trever Solven".to_string());
+    return  opt1;
+}
+
+// Game example 
+
+pub fn test_option_chartype()->Option<CharacterType>{
+    let mut chartype: Option<CharacterType> = None;
+    chartype = Some(CharacterType::Mage);
+    return  chartype;
+}
+
+
+ pub enum  CharacterType {
+    Archer,
+    Warrior, 
+    Mage
+}
+
+impl ToString for CharacterType{
+    fn to_string(&self) -> String {
+        match &self {
+            CharacterType::Archer=>"Archer",
+            CharacterType::Mage => "Mage",
+            CharacterType::Warrior=> "Warrior"
+        }.to_string()
+    }
+}
